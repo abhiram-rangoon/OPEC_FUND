b3d84a3a9f9c12ca319447178d1cae125802e51d
Abhiram Rangoon <abhiramrangoon@Abhirams-MacBook-Air-4.local>
2026-06-21 19:41:24 +0530

    done

--- unified-app/capacitor.config.ts
+++ unified-app/capacitor.config.ts
@@ -2,10 +2,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.opec.oilfund',
-  appName: 'OPEC OilFund',
+  appName: 'OPEC',
   webDir: 'out',
   server: {
-    url: 'http://192.168.1.22:3000',
+    url: 'http://192.168.0.7:3000',
     cleartext: true
   }
 };
